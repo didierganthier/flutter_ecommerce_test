2efd21d587110e826f1b1303be1bdc6e786a6dc2
didierganthier <didierganthierperan@gmail.com>
2020-09-15 15:51:12 -0400

    CustomBtn reusable widget called in login_page.dart

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_test/constants.dart';
+import 'package:flutter_ecommerce_test/widgets/custom_btn.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("LoginPage", style: Constants.regularHeading,),
+        body: SafeArea(
+      child: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                top: 24.0,
+              ),
+              child: Text(
+                "Welcome User,\nLogin to your account",
+                textAlign: TextAlign.center,
+                style: Constants.boldHeading,
+              ),
+            ),
+            Column(
+              children: [
+                Text("Inputs"),
+                Text("Inputs"),
+                Text("Inputs"),
+              ],
+            ),
+            CustomBtn(
+              text: "Create New Account",
+              onPressed:(){
+                print("Clicked the Create Account Button");
+              },
+              outlineBtn: true,
+            ),
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
