9d135272a03f6022bb087a7c6f5e4ebc86d228a9
didierganthier <didierganthierperan@gmail.com>
2020-09-15 16:11:27 -0400

    bottomPadding bug when keyboard comes up fixed

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,44 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_test/constants.dart';
 import 'package:flutter_ecommerce_test/widgets/custom_btn.dart';
+import 'package:flutter_ecommerce_test/widgets/custom_input.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Container(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              padding: EdgeInsets.only(
-                top: 24.0,
+      resizeToAvoidBottomPadding: false,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.only(
+                  top: 24.0,
+                ),
+                child: Text(
+                  "Welcome User,\nLogin to your account",
+                  textAlign: TextAlign.center,
+                  style: Constants.boldHeading,
+                ),
               ),
-              child: Text(
-                "Welcome User,\nLogin to your account",
-                textAlign: TextAlign.center,
-                style: Constants.boldHeading,
+              Column(
+                children: [
+                  CustomInput(),
+                  CustomInput(),
+                  CustomBtn(
+                    text: "Login",
+                    onPressed: () {
+                      print("Clicked the Login Button");
+                    },
+                    outlineBtn: false,
+                  )
+                ],
               ),
-            ),
-            Column(
-              children: [
-                Text("Inputs"),
-                Text("Inputs"),
-                Text("Inputs"),
-              ],
-            ),
-            CustomBtn(
-              text: "Create New Account",
-              onPressed:(){
-                print("Clicked the Create Account Button");
-              },
-              outlineBtn: true,
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.only(
+                  bottom: 16.0,
+                ),
+                child: CustomBtn(
+                  text: "Create New Account",
+                  onPressed: () {
+                    print("Clicked the Create Account Button");
+                  },
+                  outlineBtn: true,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
