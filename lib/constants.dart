791abc849413ed04cc0275f97b250b2d4e930fb1
didierganthier <didierganthierperan@gmail.com>
2020-09-15 15:26:23 -0400

    styles added in constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,5 +8,11 @@ class Constants {
     color: Colors.black,
   );
 
+  static const boldHeading = TextStyle(
+    fontSize: 22.0,
+    fontWeight: FontWeight.w600,
+    color: Colors.black,
+  );
+
 
 }
