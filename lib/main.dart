8d522c9cdf47172c869c2219ee81a13ecc36368a
didierganthier <didierganthierperan@gmail.com>
2020-09-15 14:33:37 -0400

    landing_page.dart basic setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_test/screens/landing_page.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -20,36 +20,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LandingPage extends StatelessWidget {
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot){
-        if(snapshot.hasError){
-          return Scaffold(
-            body: Text('Error ${snapshot.error}'),
-          );
-        }
-        if(snapshot.connectionState == ConnectionState.done){
-          return Scaffold(
-            body: Container(
-              child: Center(
-                child: Text("Firebase App Initialized"),
-              ),
-            ),
-          );
-        }
-        return Scaffold(
-          body: Container(
-            child: Center(
-              child: Text("Initialization..."),
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
---
+++ lib/screens/landing_page.dart
@@ -0,0 +1,36 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot){
+        if(snapshot.hasError){
+          return Scaffold(
+            body: Text('Error ${snapshot.error}'),
+          );
+        }
+        if(snapshot.connectionState == ConnectionState.done){
+          return Scaffold(
+            body: Container(
+              child: Center(
+                child: Text("Firebase App Initialized"),
+              ),
+            ),
+          );
+        }
+        return Scaffold(
+          body: Container(
+            child: Center(
+              child: Text("Initialization..."),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
