6de517e4d9a14d80a2b8f786d6bca274990fdc19
didierganthier <didierganthierperan@gmail.com>
2020-09-15 14:56:11 -0400

    login_page.dart and home_page.dart basic setup

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_test/constants.dart';
 
-class HomePage extends StatelessWidget {
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("HomePage", style: Constants.regularHeading,),
+        child: Text("LoginPage", style: Constants.regularHeading,),
       ),
     );
   }
