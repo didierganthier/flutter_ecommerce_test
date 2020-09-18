31131abd690b7f952fe01eb91e737e4b63fc0e15
didierganthier <didierganthierperan@gmail.com>
2020-09-18 19:03:59 -0400

    custom_action_bar.dart reusable widget called in home_tab.dart

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_test/widgets/custom_action_bar.dart';
 
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text("Home Page"),
+      child: Stack(
+        children: [
+          Center(
+            child: Text("Home Tab"),
+          ),
+          CustomActionBar(
+            title: "Home",
+            hasTitle: true,
+            hasBackArrow: false,
+          ),
+        ],
       ),
     );
   }
