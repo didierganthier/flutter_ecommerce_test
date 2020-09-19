3ad51fe61f04d0eb53745d4ea6466d31fa45e5af
didierganthier <didierganthierperan@gmail.com>
2020-09-19 13:40:27 -0400

    custom_action_bar.dart modified (UI)

--- lib/widgets/custom_action_bar.dart
+++ lib/widgets/custom_action_bar.dart
@@ -14,11 +14,21 @@ class CustomActionBar extends StatelessWidget {
     bool _hasTitle = hasTitle ?? true;
 
     return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.white,
+            Colors.white.withOpacity(0.5)
+          ],
+          begin: Alignment(0, 0),
+          end: Alignment(0, 1)
+        )
+      ),
       padding: EdgeInsets.only(
         top: 56.0,
         bottom: 24.0,
         left: 24.0,
-        right: 24.0,
+        right: 46.0,
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
