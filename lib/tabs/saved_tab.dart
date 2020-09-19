26cfbdee900ca9ebd72f242d0966ca0737197d69
didierganthier <didierganthierperan@gmail.com>
2020-09-19 12:30:44 -0400

    custom_action_bar.dart reusable widget called in saved_tab.dart

--- lib/tabs/saved_tab.dart
+++ lib/tabs/saved_tab.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_test/widgets/custom_action_bar.dart';
 
 class SavedTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text("Saved Page"),
+      child: Stack(
+        children: [
+          Center(
+            child: Text("Saved Page"),
+          ),
+          CustomActionBar(
+            title: "Saved",
+            hasBackArrow: false,
+          )
+        ],
       ),
     );
   }
