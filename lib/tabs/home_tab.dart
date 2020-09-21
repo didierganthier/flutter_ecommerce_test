f2bd0bb8576651a6248436f47616e3f5daa64188
didierganthier <didierganthierperan@gmail.com>
2020-09-21 12:19:45 -0400

    home_tab.dart page designed

--- lib/tabs/home_tab.dart
+++ lib/tabs/home_tab.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_test/constants.dart';
 import 'package:flutter_ecommerce_test/widgets/custom_action_bar.dart';
 
 class HomeTab extends StatelessWidget {
-  final CollectionReference _productsRef = FirebaseFirestore.instance.collection("Products");
+  final CollectionReference _productsRef =
+  FirebaseFirestore.instance.collection("Products");
 
   @override
   Widget build(BuildContext context) {
@@ -12,38 +14,84 @@ class HomeTab extends StatelessWidget {
         children: [
           FutureBuilder<QuerySnapshot>(
             future: _productsRef.get(),
-            builder: (context, snapshot){
-              if(snapshot.hasError){
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
                 return Scaffold(
                   body: Center(
-                    child: Text("Error ${snapshot.error}"),
+                    child: Text("Error: ${snapshot.error}"),
                   ),
                 );
               }
-              if(snapshot.connectionState == ConnectionState.done){
+
+              // Collection Data ready to display
+              if (snapshot.connectionState == ConnectionState.done) {
+                // Display the data inside a list view
                 return ListView(
-                  padding: EdgeInsets.only(top: 108.0, bottom: 12.0),
-                  children: snapshot.data.docs.map((document){
-                    return Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12.0)
-                      ),
-                      height: 300.0,
-                      padding: EdgeInsets.symmetric(
-                        vertical: 12.0,
-                        horizontal: 24.0,
-                      ),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(12.0),
-                        child: Image.network(
-                          "${document.data()['images'][0]}",
-                          fit: BoxFit.cover,
+                  padding: EdgeInsets.only(
+                    top: 108.0,
+                    bottom: 12.0,
+                  ),
+                  children: snapshot.data.docs.map((document) {
+                    return GestureDetector(
+                      onTap: () {
+
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12.0),
                         ),
-                      )
+                        height: 350.0,
+                        margin: EdgeInsets.symmetric(
+                          vertical: 12.0,
+                          horizontal: 24.0,
+                        ),
+                        child: Stack(
+                          children: [
+                            Container(
+                              height: 350.0,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(12.0),
+                                child: Image.network(
+                                  "${document.data()['images'][0]}",
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              bottom: 0,
+                              left: 0,
+                              right: 0,
+                              child: Padding(
+                                padding: const EdgeInsets.only(bottom: 4.0, right: 20.0, left: 20.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                  MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      document.data()['name'] ?? "Product Name",
+                                      style: Constants.regularHeading,
+                                    ),
+                                    Text(
+                                      "\$${document.data()['price']}" ?? "Price",
+                                      style: TextStyle(
+                                          fontSize: 18.0,
+                                          color: Theme.of(context).accentColor,
+                                          fontWeight: FontWeight.w600
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     );
                   }).toList(),
                 );
               }
+
+              // Loading State
               return Scaffold(
                 body: Center(
                   child: CircularProgressIndicator(),
@@ -53,7 +101,6 @@ class HomeTab extends StatelessWidget {
           ),
           CustomActionBar(
             title: "Home",
-            hasTitle: true,
             hasBackArrow: false,
           ),
         ],
