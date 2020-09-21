93c9d4b9e09d64d5b5caee4ba26fa61b9ac3e3de
didierganthier <didierganthierperan@gmail.com>
2020-09-21 16:53:42 -0400

    back function activated in custom_action_bar.dart

--- lib/widgets/custom_action_bar.dart
+++ lib/widgets/custom_action_bar.dart
@@ -6,8 +6,14 @@ class CustomActionBar extends StatelessWidget {
   final bool hasTitle;
   final bool hasBackArrow;
   final bool hasBackground;
+  final BuildContext context;
 
-  CustomActionBar({this.title, this.hasBackArrow, this.hasTitle, this.hasBackground});
+  CustomActionBar(
+      {this.title,
+      this.hasBackArrow,
+      this.hasTitle,
+      this.hasBackground,
+      this.context});
 
   @override
   Widget build(BuildContext context) {
@@ -17,14 +23,12 @@ class CustomActionBar extends StatelessWidget {
 
     return Container(
       decoration: BoxDecoration(
-        gradient: _hasBackground? LinearGradient(
-          colors: [
-            Colors.white,
-            Colors.white.withOpacity(0.5)
-          ],
-          begin: Alignment(0, 0),
-          end: Alignment(0, 1)
-        ) : null,
+        gradient: _hasBackground
+            ? LinearGradient(
+                colors: [Colors.white, Colors.white.withOpacity(0.5)],
+                begin: Alignment(0, 0),
+                end: Alignment(0, 1))
+            : null,
       ),
       padding: EdgeInsets.only(
         top: 56.0,
@@ -35,18 +39,29 @@ class CustomActionBar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          if(_hasBackArrow)
+          if (_hasBackArrow)
             Container(
-                height: 42,
-                width: 42,
-                decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.all(Radius.circular(8.0))
+              height: 42,
+              width: 42,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(8.0),
                 ),
-                alignment: Alignment.center,
-              child: IconButton(icon: Icon(Icons.arrow_back_ios, color: Colors.white, size: 18,), onPressed:(){},),
               ),
-          if(_hasTitle)
+              alignment: Alignment.center,
+              child: IconButton(
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.white,
+                  size: 18,
+                ),
+                onPressed:(){
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+          if (_hasTitle)
             Text(
               title ?? "Action Bar",
               style: Constants.boldHeading,
@@ -55,9 +70,8 @@ class CustomActionBar extends StatelessWidget {
             height: 42,
             width: 42,
             decoration: BoxDecoration(
-              color: Colors.black,
-              borderRadius: BorderRadius.all(Radius.circular(8.0))
-            ),
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(8.0))),
             alignment: Alignment.center,
             child: Text(
               "0",
