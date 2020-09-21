4fc3006febcbe9455aa33ab24e8f6af5b1bc1b46
didierganthier <didierganthierperan@gmail.com>
2020-09-21 16:55:04 -0400

    product_size.dart reusable widget created

---
+++ lib/widgets/product_size.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_test/constants.dart';
+
+class ProductSize extends StatefulWidget {
+  final List productSizes;
+
+  ProductSize({this.productSizes});
+
+  @override
+  _ProductSizeState createState() => _ProductSizeState();
+}
+
+class _ProductSizeState extends State<ProductSize> {
+  int _selected = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(
+        left: 20.0,
+      ),
+      child: Row(
+        children: [
+          for (var i = 0; i < widget.productSizes.length; i++)
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  _selected = i;
+                });
+              },
+              child: Container(
+                width: 42,
+                height: 42,
+                decoration: BoxDecoration(
+                  color: _selected == i ? Theme.of(context).accentColor : Color(0xFFDCDCDC),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                alignment: Alignment.center,
+                margin: EdgeInsets.symmetric(
+                  horizontal: 4.0,
+                ),
+                child: Text(
+                  "${widget.productSizes[i]}",
+                  style: Constants.regularHeading.copyWith(
+                    fontSize: 16.0,
+                    color: _selected == i ? Colors.white : Colors.black,
+                  ),
+                ),
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
