751cda96b8a77b7a9287846258f896a806fb1653
didierganthier <didierganthierperan@gmail.com>
2020-10-07 15:46:22 -0400

    search feature implemented in search_tab.dart

--- lib/tabs/search_tab.dart
+++ lib/tabs/search_tab.dart
@@ -1,11 +1,79 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_test/constants.dart';
+import 'package:flutter_ecommerce_test/services/firebase_services.dart';
+import 'package:flutter_ecommerce_test/widgets/custom_input.dart';
+import 'package:flutter_ecommerce_test/widgets/product_card.dart';
 
-class SearchTab extends StatelessWidget {
+class SearchTab extends StatefulWidget {
+  @override
+  _SearchTabState createState() => _SearchTabState();
+}
+
+class _SearchTabState extends State<SearchTab> {
+  FirebaseServices _firebaseServices = FirebaseServices();
+
+  String search = "";
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: Text("Search Page"),
+      child: Stack(
+        children: [
+          FutureBuilder<QuerySnapshot>(
+            future: _firebaseServices.productsRef
+                .orderBy("name")
+                .startAt([search]).endAt(["$search\uf8ff"]).get(),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Scaffold(
+                  body: Center(
+                    child: Text("Error: ${snapshot.error}"),
+                  ),
+                );
+              }
+
+              // Collection Data ready to display
+              if (snapshot.connectionState == ConnectionState.done) {
+                // Display the data inside a list view
+                return ListView(
+                  padding: EdgeInsets.only(
+                    top: 128.0,
+                    bottom: 12.0,
+                  ),
+                  children: snapshot.data.docs.map((document) {
+                    return ProductCard(
+                      title: document.data()['name'],
+                      imageUrl: document.data()['images'][0],
+                      price: document.data()['price'].toString(),
+                      productID: document.id,
+                    );
+                  }).toList(),
+                );
+              }
+
+              // Loading State
+              return Scaffold(
+                body: Center(
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            },
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 45.0),
+            child: CustomInput(
+              hintText: "Search here...",
+              onSubmitted: (value) {
+                if (value.isNotEmpty) {
+                  setState(() {
+                    search = value;
+                  });
+                }
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
